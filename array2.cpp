6bb158303f5f8919576cd4f2434a9cf7bcea5413
Shivansh Joshi <shivanshjoshi2922@gamil.com>
2025-03-29 17:57:26 +0530

    rearrange_elements_by_sign_part_2, rearrange_elements_by_sign

--- array2.cpp
+++ array2.cpp
@@ -1,12 +1,99 @@
 #include <bits/stdc++.h>
 using namespace std;
+void rearrange_elements_by_sign_part_2(vector<int> &arr, int n)
+{
+    // vector<int> ans(n);
+    vector<int> pos;
+    vector<int> neg;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            pos.push_back(arr[i]);
+        else
+            neg.push_back(arr[i]);
+    }
+
+    if (pos.size() > neg.size())
+    {
+        for (int i = 0; i < neg.size(); i++)
+        {
+            arr[2 * i] = pos[i];
+            arr[2 * i + 1] = neg[i];
+        }
+
+        int idx = neg.size() * 2;
+        for (int i = neg.size(); i < pos.size(); i++)
+        {
+            arr[idx] = pos[i];
+            idx++;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < pos.size(); i++)
+        {
+            arr[2 * i] = pos[i];
+            arr[2 * i + 1] = neg[i];
+        }
+
+        int idx = pos.size() * 2;
+        for (int i = pos.size(); i < neg.size(); i++)
+        {
+            arr[idx] = neg[i];
+            idx++;
+        }
+    }
+
+    // int k = 0;
+    // for (int num : ans)
+    // {
+    //     arr[k] = num;
+    //     k++;
+    // }
+}
+
+void rearrange_elements_by_sign(vector<int> &arr, int n)
+{
+    // optimal
+    int posIdx = 0;
+    int negIdx = 1;
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+        {
+            ans[posIdx] = arr[i];
+            posIdx += 2;
+        }
+        else
+        {
+            ans[negIdx] = arr[i];
+            negIdx += 2;
+        }
+    }
+    int k = 0;
+    for (int num : ans)
+    {
+        arr[k] = num;
+        k++;
+    }
+}
 
 int best_time_to_buy_and_sell_stocks(vector<int> &arr, int n)
 {
-    
+    int minPrice = arr[0];
+    int maxProfit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int cost = arr[i] - minPrice;
+        maxProfit = max(maxProfit, cost);
+        minPrice = min(minPrice, arr[i]);
+    }
+    return maxProfit;
 }
 
-int maximum_subarray_sum_length_kadanes_algo(vector<int> &arr, int n)
+int maximum_subarray_sum_length_kadanes_algorithm(vector<int> &arr, int n)
 {
     // optimal
     int maxSum = INT_MIN, currSum = 0, start = 0, end = 0, tempStart = 0;
@@ -123,7 +210,11 @@ int main()
         cin >> arr[i];
     }
     // cout << majority_element(arr, n) << endl;
-    // cout << maximum_subarray_sum_length_kadanes_algo(arr, n);
-
-    cout << best_time_to_buy_and_sell_stocks(arr, n) << endl;
+    // cout << maximum_subarray_sum_length_kadanes_algorithm(arr, n);
+    // cout << best_time_to_buy_and_sell_stocks(arr, n) << endl;
+    rearrange_elements_by_sign_part_2(arr, n);
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
 }
